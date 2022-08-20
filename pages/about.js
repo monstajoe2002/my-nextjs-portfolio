5a8a149f748a5b49ebb12eecddd7ea0fea089309
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-08-20 23:34:26 +0200

    finished about page

--- pages/about.js
+++ pages/about.js
@@ -4,20 +4,23 @@ export default function About() {
   return (<>
     <div className="text-dark-blue-50 p-60 text-5xl font-semibold leading-[1.25] justify-start">
       About Me
-    <div className="text-xl text-light-blue-100 font-semibold">
-        We're no strangers to love
-        You know the rules and so do I (do I)
-        A full commitment's what I'm thinking of
-        You wouldn't get this from any other guy
-        I just wanna tell you how I'm feeling
-        Gotta make you understand
-        Never gonna give you up
-        Never gonna let you down
-        Never gonna run around and desert you
-        Never gonna make you cry
-        Never gonna say goodbye
-        Never gonna tell a lie and hurt you
-    </div>
+      <div className="text-xl text-light-blue-100 font-semibold pr-8 fl">
+        My name is Youssef, 20 years old. I'm a third-year computer science student
+        at the GIUAS (German Internaional University of Applied Sciences).
+        Throughout my CS education, I have been working on projects that are web-based
+        as well as system-based. I have a passion for software development and I'm
+        always looking for new ways to improve my skills. Currently, I am proficient in
+        the following languages:
+        <div className=" text-dark-blue-100 inline">
+          &nbsp;JavaScript, HTML, CSS, Python, C, C++, Java, and SQL.&nbsp;
+        </div>
+        
+          My hobbies include:
+          <div className=" text-dark-blue-100 inline">
+            &nbsp;playing PC games, programming, music and binge-watching Criminal Minds.&nbsp;
+          </div>
+        
+      </div>
     </div>
   </>
   )
