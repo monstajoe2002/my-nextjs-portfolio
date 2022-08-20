d0c4a2d290cbc872a622cc66d63593651df0c7ea
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-08-20 21:42:20 +0200

    finalized homepage

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ export default function Home() {
         <title>My portfolio</title>
         <meta name="og:title" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="text-light-blue-100 m-60 text-5xl font-semibold leading-[1.25]">
+      <div className="text-light-blue-100 p-60 w-auto h-auto text-5xl font-semibold leading-[1.25]">
         Hello world,<br/> my name is <br/><div className="text-dark-blue-100">Youssef Amr.</div>
+      <div className="text-sm text-black font-normal">
+        A.K.A <strong>MONSTAJOE</strong>*
       </div>
+      </div>
+      
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
-      'light-blue-50':'#E8F9FD',
-      'light-blue-100':'#79DAE8',
-      'dark-blue-50': '#0AA1DD',
-      'dark-blue-100': '#2155CD',
-    },
     extend: {
+      colors:{
+        'light-blue-50':'#E8F9FD',
+        'light-blue-100':'#79DAE8',
+        'dark-blue-50': '#0AA1DD',
+        'dark-blue-100': '#2155CD',
+      },
       fontFamily:{
         sans:['Inter','sans-serif'],
       }
