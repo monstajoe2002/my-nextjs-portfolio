8eab76665d11655b905e05c6ce151023d706c656
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-08-22 15:50:01 +0200

    added comment

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,16 @@ export default function Home() {
         <meta name="og:title" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="text-light-blue-100 p-60 justify-start text-5xl font-semibold leading-[1.25]">
-        Hello world,<br/> my name is <br/><div className="text-dark-blue-100">Youssef Amr.</div>
-      <div className="text-sm text-black font-normal">
-        A.K.A <strong>MONSTAJOE</strong>*
+        Hello world,<br /> my name is <br /><div className="text-dark-blue-100">Youssef Amr.</div>
+        <div className="text-sm text-black font-normal">
+          A.K.A <strong>MONSTAJOE</strong>*
+        </div>
+        <div className="text-sm text-black font-normal">
+          *Yes, my name is all caps because I <strong>LIKE</strong> it bb. <br/>
+          Also, I used to produce music so this was my handle.
+        </div>
       </div>
-      </div>
-      
+
     </>
   )
 }
