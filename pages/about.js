3c7320efbce45e603a61d0557030907692a99875
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-08-23 02:14:21 +0200

    fixed build errors

--- pages/about.js
+++ pages/about.js
@@ -5,10 +5,10 @@ export default function About() {
     <div className="text-dark-blue-50 p-60 text-5xl font-semibold leading-[1.25] justify-start">
       About Me
       <div className="text-xl text-light-blue-100 font-semibold pr-8 fl">
-        My name is Youssef, 20 years old. I'm a third-year computer science student
+        My name is Youssef, 20 years old. I am a third-year computer science student
         at the GIUAS (German Internaional University of Applied Sciences).
         Throughout my CS education, I have been working on projects that are web-based
-        as well as system-based. I have a passion for software development and I'm
+        as well as system-based. I have a passion for software development and I am
         always looking for new ways to improve my skills. Currently, I am proficient in
         the following languages:
         <div className=" text-dark-blue-100 inline">
